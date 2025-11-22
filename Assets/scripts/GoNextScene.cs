8e3f407f2627030dbd5fee06705f3e91a53a70c2
zdz10124@163.com <zdz10124@163.com>
2025-11-22 23:17:24 +0800

    完成了开始游戏按钮和退出按钮，开始游戏界面大致完成

---
+++ Assets/scripts/GoNextScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoNextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void NextScene()//加载下一个场景
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;//最后一个的下一个是第一个
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
